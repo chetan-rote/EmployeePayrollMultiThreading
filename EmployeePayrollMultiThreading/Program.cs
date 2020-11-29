8367e50b2ad4c418068b00a3060e302b9186a253
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-29 14:16:15 +0530

    Add project files.

---
+++ EmployeePayrollMultiThreading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollMultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll using MultiThreading.");
+        }
+    }
+}
